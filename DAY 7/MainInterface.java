57ef1ec628233bfd7094558e48987a76929e49bf
Ankit Gupta <iankitgupta04@gmail.com>
2026-02-16 12:09:58 +0530

    fix

--- DAY 7/MainInterface.java
+++ DAY 7/MainInterface.java
@@ -13,6 +13,11 @@ class Sum implements Calc {
         int s = n1 + n2;
         System.out.println("Sum = " + s);
     }
+
+    public void calculate(int n1, int n2, int n3) {
+        int s = n1 + n2 + n3;
+        System.out.println("Sum of 3 numbers :  " + s);
+    }
 }
 
 public class MainInterface {
