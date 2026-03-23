8995b7fca43a491fbd024c49b6e8150f7294f1dd
Ankit Gupta <iankitgupta04@gmail.com>
2026-03-23 11:57:48 +0530

    fix

---
+++ Map1.java
@@ -0,0 +1,7 @@
+import java.util.*;
+public class Map1 {
+    public static void main(String[] args) {
+        List<String> name = Arrays.asList("sachin","rahul","amit");
+        
+    }
+}
---
+++ ReduceEx.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class ReduceEx {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(34, 12, 45, 76, 34, 56, 54, 32);
+        int sum = list.stream().reduce(1, (a, b) -> a + b);
+        System.out.println("Sum = " + sum);
+        double avg = list.stream().reduce(0, (a, b) -> a + b) / list.size();
+        System.out.println("Avg = " + avg);
+    }
+}
