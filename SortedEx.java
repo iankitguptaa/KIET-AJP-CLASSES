7f30ac2d11cc3c74365b88198e2db549e7a3fc8e
Ankit Gupta <iankitgupta04@gmail.com>
2026-03-24 09:32:46 +0530

    fix

--- SortedEx.java
+++ SortedEx.java
@@ -6,6 +6,8 @@ public class SortedEx {
         List<Integer> list = Arrays.asList(23, 12, 1, 45, 23, 76, 26, 91);
         System.out.println(list);
         List<Integer> sortedList = list.stream().sorted().toList();
+        List<Integer> sortedListdesc = list.stream().sorted((a, b) -> b - a).toList();
         System.out.println(sortedList);
+        System.out.println(sortedListdesc);
     }
 }
